60899ee836e869df90de756376af2ba0b9cc39cc
Anton <Spawni777@gmail.com>
2019-11-27 03:04:19 +0300

    add condition when gold > 120

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -170,8 +170,8 @@
                 } else if (neededGold > 120) {
                     this.h2List.push('Difference between levels are too low!');
                     this.h2List.push('Just roll now!');
+                    console.log('Difference between levels are too low!');
                     console.log('Just roll now!');
-                    console.log('Just roll now! Difference between levels are too low!');
                     return;
                 }
 
